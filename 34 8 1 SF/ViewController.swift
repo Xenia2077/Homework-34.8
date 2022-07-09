70488b3e68b199f5fb92b691c9fe13fecfe5b2f5
Ксения Борисова <freuleingast@gmail.com>
2022-07-09 17:53:38 +0300

    Initial Commit

---
+++ 34 8 1 SF/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  34 8 1 SF
+//
+//  Created by Ксения Борисова on 09.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
