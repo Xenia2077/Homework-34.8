7d230551b2ff4bc66e261e9da93c63fb08de82bf
Ксения Борисова <freuleingast@gmail.com>
2022-07-09 19:20:21 +0300

    Создала приложение по заданию

--- 34 8 1 SF/ViewController.swift
+++ 34 8 1 SF/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    func getPosts(from url: URL?) {
+        guard let url = url else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+     
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data,
+                let dataString = String(data: data, encoding: .utf8),
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                error == nil else { return }
+            DispatchQueue.main.async {
+                self.textView.text = dataString
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func okBtn(_ sender: UIButton) {
+        let numberOfPost = textField.text
+        let url = URL(string: "https://jsonplaceholder.typicode.com/comments?postId=\(numberOfPost ?? "1")")
+        getPosts(from: url)
+        
+    }
+    
 }
 
